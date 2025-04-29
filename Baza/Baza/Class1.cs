20a6f73074584b1d644fe71e061c2617779365c4
Zheniya <zheniya>
2025-04-29 22:07:00 +0300

    29/04 add function in dll by zhenya

--- Baza/Baza/Class1.cs
+++ Baza/Baza/Class1.cs
@@ -37,5 +37,22 @@
             return 2 * (width + height);
         }
 
+        // Методы для пирамиды 
+
+        public double PyramidBaseArea(double baseLength, double baseWidth) => baseLength * baseWidth;
+
+        public double PyramidVolume(double baseLength, double baseWidth, double height)
+            => (baseLength * baseWidth * height) / 3;
+
+        public double PyramidLateralArea(double baseLength, double baseWidth, double height)
+        {
+            double apothemLength = Math.Sqrt(Math.Pow(height, 2) + Math.Pow(baseLength / 2, 2));
+            double apothemWidth = Math.Sqrt(Math.Pow(height, 2) + Math.Pow(baseWidth / 2, 2));
+            return baseLength * apothemWidth + baseWidth * apothemLength;
+        }
+
+        public double PyramidTotalArea(double baseLength, double baseWidth, double height)
+            => PyramidBaseArea(baseLength, baseWidth) + PyramidLateralArea(baseLength, baseWidth, height);
+
     }
 }
--- Baza/Baza/obj/Debug/net8.0/Baza.AssemblyInfo.cs
+++ Baza/Baza/obj/Debug/net8.0/Baza.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Baza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+928190251c3fbd04a2235b27575322a55007b4a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Baza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Baza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
